5b4f5219bfa5cbb67f047f61257332109a9277dd
SalamiArmy <ultrakiff@gmail.com>
2016-05-19 10:32:47 +0200

    /define is kinda broken, this patch is rather untested

--- PyBoet.py
+++ PyBoet.py
@@ -7,7 +7,6 @@ import string
 from dateutil import tz
 
 import telegram
-import json
 import urllib
 import urllib2
 import random
@@ -17,7 +16,7 @@ import datetime
 import soundcloud
 import feedparser
 import tungsten
-import requests
+import xmltodict
 
 #reverse image search imports:
 from bs4 import BeautifulSoup
@@ -395,18 +394,18 @@ def getUpdatesLoop(bot, keyConfig, lastUserWhoMoved):
                                                           ', you\'re just too filthy.')
 # -------------------------------------------Dictionary : DictionaryAPI.net---------------------------------------------
             elif dicType:
-                dicUrl = 'http://dictionaryapi.net/api/definition/'
-                realUrl = dicUrl + requestText.encode('utf-8')
-                data = json.load(urllib.urlopen(realUrl))
-                if len(data) >= 1:
-                    partOfSpeech = data[random.randint(0, len(data) - 1)]
-                    if len(partOfSpeech['Definitions']) >= 1:
-                        definitionText = partOfSpeech['Definitions'][0]
+                dicUrl = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'
+                realUrl = dicUrl + requestText.encode('utf-8') + '?key=' + keyConfig.get('Merriam-Webster', 'API_KEY')
+                data = xmltodict.parse(urllib.urlopen(realUrl).read())
+                if len(data['entry_list']) >= 1:
+                    partOfSpeech = data['entry_list']['entry']['fl']
+                    if len(partOfSpeech) >= 1:
+                        definitionText = data['entry_list']['entry']['def']['dt'][0]
                         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
                         userWithCurrentChatAction = chat_id
                         urlForCurrentChatAction = (user + ': ' if not user == '' else '') +\
                                                   requestText.title() + ":\n" + \
-                                                  partOfSpeech['PartOfSpeech'] + ".\n\n" + definitionText
+                                                  partOfSpeech + ".\n\n" + definitionText
                         bot.sendMessage(chat_id=chat_id, text=urlForCurrentChatAction)
                     else:
                         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
@@ -422,10 +421,39 @@ def getUpdatesLoop(bot, keyConfig, lastUserWhoMoved):
                                               ', I\'m afraid I can\'t find any definitions for the word ' +\
                                               requestText + '.'
                     bot.sendMessage(chat_id=userWithCurrentChatAction, text=urlForCurrentChatAction)
+
+                ############################# Ashley: http://dictionaryapi.net/ is down! ###############################
+                # dicUrl = 'http://dictionaryapi.net/api/definition/'
+                # realUrl = dicUrl + requestText.encode('utf-8')
+                # data = json.load(urllib.urlopen(realUrl))
+                # if len(data) >= 1:
+                #     partOfSpeech = data[random.randint(0, len(data) - 1)]
+                #     if len(partOfSpeech['Definitions']) >= 1:
+                #         definitionText = partOfSpeech['Definitions'][0]
+                #         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+                #         userWithCurrentChatAction = chat_id
+                #         urlForCurrentChatAction = (user + ': ' if not user == '' else '') +\
+                #                                   requestText.title() + ":\n" + \
+                #                                   partOfSpeech['PartOfSpeech'] + ".\n\n" + definitionText
+                #         bot.sendMessage(chat_id=chat_id, text=urlForCurrentChatAction)
+                #     else:
+                #         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+                #         userWithCurrentChatAction = chat_id
+                #         urlForCurrentChatAction = 'I\'m sorry ' + (user if not user == '' else 'Dave') +\
+                #                                   ', I\'m afraid I can\'t find any definitions for the word ' +\
+                #                                   requestText + '.'
+                #         bot.sendMessage(chat_id=userWithCurrentChatAction, text=urlForCurrentChatAction)
+                # else:
+                #     bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+                #     userWithCurrentChatAction = chat_id
+                #     urlForCurrentChatAction = 'I\'m sorry ' + (user if not user == '' else 'Dave') +\
+                #                               ', I\'m afraid I can\'t find any definitions for the word ' +\
+                #                               requestText + '.'
+                #     bot.sendMessage(chat_id=userWithCurrentChatAction, text=urlForCurrentChatAction)
 # -----------------------------------------------Urban Dictionary : Urban API-------------------------------------------
             elif urbanDicType:
                 dicurl = 'http://api.urbandictionary.com/v0/define?term='
-                realUrl = dicurl + requestText.encode('utf-8')
+                realUrl = dicurl + requestText.encode('utf-8') + "?key=" + keyConfig.get('Merriam-Webster', 'API_KEY')
                 data = json.load(urllib.urlopen(realUrl))
                 if 'list' in data and len(data['list']) >= 1:
                     resultNum = data['list'][random.randint(0, len(data['list']) - 1)]
