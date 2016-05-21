8d2aad0e72557e6d009f9317567ad30d311c34b9
SalamiArmy <ultrakiff@gmail.com>
2016-05-21 09:06:43 +0200

    /me command fails to find server more gracefully

--- PyBoet.py
+++ PyBoet.py
@@ -1032,13 +1032,25 @@ def getUpdatesLoop(bot, keyConfig, lastUserWhoMoved):
                 mcServer = keyConfig.get('Minecraft', 'SVR_ADDR')
                 mcPort = int(keyConfig.get('Minecraft', 'SVR_PORT'))
                 dynmapPort = keyConfig.get('Minecraft', 'DYNMAP_PORT')
-                status = MinecraftServer(mcServer, mcPort).status()
-                bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-                userWithCurrentChatAction = chat_id
-                urlForCurrentChatAction = ('The server at {0} has {1} players and replied in {2} ms' +
-                                           ('' if dynmapPort == '' else '\nSee map: ' + mcServer + ':' + dynmapPort))\
-                    .format(mcServer + ':' + str(mcPort), status.players.online, status.latency)
-                bot.sendMessage(chat_id=userWithCurrentChatAction, text=urlForCurrentChatAction)
+                error = False
+                try:
+                    status = MinecraftServer(mcServer, mcPort).status()
+                except IOError:
+                    error = True
+                if not error:
+                    bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+                    userWithCurrentChatAction = chat_id
+                    urlForCurrentChatAction = ('The server at {0} has {1} players and replied in {2} ms' +
+                                               ('' if dynmapPort == '' else '\nSee map: ' + mcServer + ':' + dynmapPort))\
+                        .format(mcServer + ':' + str(mcPort), status.players.online, status.latency)
+                    bot.sendMessage(chat_id=userWithCurrentChatAction, text=urlForCurrentChatAction)
+                else:
+                    bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+                    userWithCurrentChatAction = chat_id
+                    urlForCurrentChatAction = 'I\'m sorry ' + (user if not user == '' else 'Dave') +\
+                                              ', I\'m afraid I cannot find the Minecraft server at ' +\
+                                              mcServer + ':' + str(mcPort)
+                    bot.sendMessage(chat_id=chat_id, text=urlForCurrentChatAction)
 # --------------------------------------------------Current Proteas match-----------------------------------------------
             elif cricType:
                 allMatchesUrl = 'http://cricscore-api.appspot.com/csa'
